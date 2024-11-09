452c5271622b6701f95f70272258b3e0ea7fddf0
TWorker <157353301+TWorkerFrog@users.noreply.github.com>
2024-11-09 20:35:01 +0500

    Very very cool  UI (I made all project by solo)

--- source/AnimaFiltering/AnimaFiltering/ViewModels/MainViewModel.cs
+++ source/AnimaFiltering/AnimaFiltering/ViewModels/MainViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace AnimaFiltering.ViewModels;
+﻿using System.Collections.ObjectModel;
+//using Avalonia.Xaml.Interactions.DragAndDrop;
+using Avalonia.Input;
+using Avalonia.Platform.Storage;
 
-public partial class MainViewModel : ViewModelBase
+namespace AnimaFiltering.ViewModels
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<AttachmentLink> Attachments { get; } = new ObservableCollection<AttachmentLink>();
+
+        public void DropHandler(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetFiles() is { } fileNames)
+            {
+                foreach (var file in fileNames)
+                {
+                    Attachments.Add(new AttachmentLink(file));
+                }
+            }
+        }
+    }
+
+    public class AttachmentLink
+    {
+        public IStorageItem FilePath { get; }
+
+        public AttachmentLink(IStorageItem filePath)
+        {
+            FilePath = filePath;
+        }
+    }
 }
--- source/AnimaFiltering/AnimaFiltering/Views/MainView.axaml.cs
+++ source/AnimaFiltering/AnimaFiltering/Views/MainView.axaml.cs
@@ -1,11 +1,35 @@
-﻿using Avalonia.Controls;
-
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using AnimaFiltering.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 namespace AnimaFiltering.Views;
 
 public partial class MainView : UserControl
 {
+
     public MainView()
     {
         InitializeComponent();
+        //AddHandler(DragDrop.DragOverEvent, DragOver);
+        AddHandler(DragDrop.DropEvent, Drop);
     }
+
+    private void Drop(object? sender, DragEventArgs e)
+    {
+        var files = e.Data.GetFiles();
+        if (files!= null)
+        {
+            foreach (var file in files)
+            {
+                Debugger.Break();
+            }
+        }
+    }
+
+
 }
